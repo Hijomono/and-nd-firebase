61f5d0f0d290ddb4aedf60936fb41e822a99bae5
Daniel Eusebio Benito Esteban <dan13use@gmail.com>
2017-03-23 20:49:21 +0100

    Add app context @Provides method, just in case ;p

--- app/src/main/java/com/google/firebase/udacity/friendlychat/android/MainModule.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/android/MainModule.java
@@ -1,13 +1,25 @@
 package com.google.firebase.udacity.friendlychat.android;
 
+import android.content.Context;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class MainModule {
+
     private final FriendlyChatApplication application;
 
     public MainModule(final FriendlyChatApplication application) {
         this.application = application;
+    }
+
+    @Singleton
+    @Provides
+    public Context provideApplicationContext() {
+        return application;
     }
 
 }
